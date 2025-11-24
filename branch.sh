89d58e30092bddabcb31d7f7ba660ec6413c5a82
Nicholas Gabriel Silva Soares <nicholas.soares@escolar.ifrn.edu.br>
2025-11-24 16:48:33 -0300

    Script - Atualizar Branchs

--- branch.sh
+++ branch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Script que automatiza a execução desses comandos git:
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 git fetch --all
 git pull --all
+#Esses comandos resumidamente vão puxar suas branchs remotas e adicionar localmente,
+#serve pra você não ter que copiar isso toda vez que abrir uma nova pasta.
